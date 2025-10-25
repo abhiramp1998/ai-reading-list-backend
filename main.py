eab13866998c1a616806adbccab6881070239bb5
Abhiramp1998 <abhiramp98@gmail.com>
2025-10-26 00:05:41 +0100

    fix: Remove dotenv import for Cloud Run

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import google.generativeai as genai
 from flask import Flask, request, jsonify
 from flask_cors import CORS # Handles browser security (CORS)
-from dotenv import load_dotenv # Import for loading .env file
+# from dotenv import load_dotenv # Import for loading .env file
 
 # Libraries for scraping web pages
 import requests
